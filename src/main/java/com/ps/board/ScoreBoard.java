80ec56bbd199fb019badc77b282afc673f9706f5
bartlomiej.zylinski <b.zylinski.o@gmail.com>
2024-06-25 21:44:56 +0200

    Added JavaDocs for interfaces

--- src/main/java/com/ps/board/ScoreBoard.java
+++ src/main/java/com/ps/board/ScoreBoard.java
@@ -1,5 +1,8 @@
 package com.ps.board;
 
+import com.ps.board.exceptions.EmptyTeamException;
+import com.ps.board.exceptions.GameIdNullException;
+import com.ps.board.exceptions.GameNotFoundException;
 import com.ps.board.model.FinishedGame;
 import com.ps.board.model.Game;
 import com.ps.board.model.NewGame;
@@ -7,13 +10,48 @@ import com.ps.board.model.ScoreBoardSummary;
 
 import java.util.UUID;
 
+/**
+ * Interface for managing football games.
+ */
 public interface ScoreBoard {
 
+    /**
+     * Starts a new game with the specified home and away teams.
+     *
+     * @param homeTeam the name of the home team
+     * @param awayTeam the name of the away team
+     * @return a {@link NewGame} object representing the newly started game
+     * @throws EmptyTeamException for null gameId value
+     */
     NewGame startGame(String homeTeam, String awayTeam);
 
+    /**
+     * Updates the score of an ongoing game.
+     *
+     * @param gameId        the unique identifier of the game
+     * @param homeTeamScore the new score for the home team
+     * @param awayTeamScore the new score for the away team
+     * @return a {@link Game} object representing the updated game
+     * @throws GameIdNullException for null gameId value
+     * @throws GameNotFoundException for non-existing game
+     */
     Game updateScore(UUID gameId, int homeTeamScore, int awayTeamScore);
 
+    /**
+     * Finishes an ongoing game.
+     *
+     * @param gameId the unique identifier of the game
+     * @return a {@link FinishedGame} object representing the finished game
+     * @throws GameIdNullException for null gameId value
+     * @throws GameNotFoundException for non-existing game
+     */
     FinishedGame finishGame(UUID gameId);
 
+    /**
+     * Retrieves a summary of all ongoing games.
+     * The games with the same total score will be returned ordered by the most recently started match in the scoreboard
+     *
+     * @return a {@link ScoreBoardSummary} object containing the summary of games in progress
+     */
     ScoreBoardSummary getSummary();
 }
--- src/main/java/com/ps/store/GamesStore.java
+++ src/main/java/com/ps/store/GamesStore.java
@@ -4,15 +4,47 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
+/**
+ * Interface for managing game states in a games store.
+ */
 public interface GamesStore {
 
+    /**
+     * Creates a new game state with the given details.
+     *
+     * @param state {@link GameState} the initial state of Game to save
+     * @return the created {@link GameState} object
+     */
     GameState create(GameState state);
 
+    /**
+     * Retrieves the game state for the specified game ID.
+     *
+     * @param gameId the unique identifier of the game
+     * @return an {@link Optional} containing the {@link GameState} if found, or empty if not found
+     */
     Optional<GameState> get(UUID gameId);
 
+    /**
+     * Updates the given game state.
+     *
+     * @param state the {@link GameState} object to update
+     * @return the updated {@link GameState} object
+     */
     GameState update(GameState state);
 
+    /**
+     * Deletes the game state for the specified game ID.
+     *
+     * @param gameId the unique identifier of the game
+     * @return the deleted {@link GameState} object
+     */
     GameState delete(UUID gameId);
 
+    /**
+     * Retrieves all games.
+     *
+     * @return a {@link Set} containing all {@link GameState} objects
+     */
     Set<GameState> getAll();
 }
